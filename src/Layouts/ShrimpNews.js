9b757b0444b163a4001c21817b81fdd8a2b437da
DEV01 <dev01@DEV01s-Mac-mini.local>
2022-05-21 17:01:10 +0700

    updated screen shrimpnews

--- src/Layouts/ShrimpNews.js
+++ src/Layouts/ShrimpNews.js
@@ -1,107 +1,146 @@
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { 
-    View,
-    Text,
-    StyleSheet,
-    Image,
-    FlatList,
- } from "react-native";
+import { View, Text, StyleSheet, Image, FlatList, Share, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import moment from "moment";
 
 const ShrimpNews = () => {
-    const getDataFromApiAsync = async () => {
-        try{
-            let response = await fetch('https://app.jala.tech/api/posts?per_page=15&page=1&with=creator', {
-                headers:{
-                    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjcwY2UyYjZjNzEzNjkyZmVjNDEwMTJjODYwMjRhMzM4M2ZiOGZjNDI0NDU3NzFmMjMwNjIyMDFkOTJlNDZlYzIxNjYwNTk3ZjdlY2I4YzU4In0.eyJhdWQiOiIxIiwianRpIjoiNzBjZTJiNmM3MTM2OTJmZWM0MTAxMmM4NjAyNGEzMzgzZmI4ZmM0MjQ0NTc3MWYyMzA2MjIwMWQ5MmU0NmVjMjE2NjA1OTdmN2VjYjhjNTgiLCJpYXQiOjE2MzU5OTYyNzEsIm5iZiI6MTYzNTk5NjI3MSwiZXhwIjoxNjY3NTMyMjcxLCJzdWIiOiI3NyIsInNjb3BlcyI6W119.F1bxyaeDygJb66xI3gDYBWmoN_JObb4jItMEtGzDFuW2w9dlA_zglk0CbqZ-keBO3a3w75Ui3t5XoE5I2lV3PP0BMZC7fFgx00sicEVrzNFlt2oUzj5n3RgpGFnclJHmnX-ObSBk-1efciBdB0PcrSMjQp4HrhQXVkzN-Xd4debnzohNOX8nhqdf3GLOoQm8Fak6nSGWy0-vsY9J4mLjzNcPtkBA5lfPk9Z_TCzNUSy1iOyE8sZHcYQSGfehcXISOL1Oev_djgyVzzbZ45jW1GDujO4d94xqY2EdDDpPuKd2bMnA9FOgfoRrxvIJ8u1AFvr6A_QBzH1kwJfWems2_jlpF91C2ZYm1LQhf4DAMoaNZhd1SDYJheh_Nx8mgeYgBgqcWv2tD-1u0-ghyTfN950NelZ_IdUxDJ_Z9riLjDrDJ96WEJNp7pxHJfc34IM5Ok4Im1ewZr8VIIGoXY5u01in_af2JWpKfeODACPO7I2xheMX6c7NInDArMBByB31CUi_NvhJtDoYAZLCeNBWsAG3AGNapl5gue2EdYj263QrfadWtDp5scGaW1f1sINLo9e_HZceLplR336MABHy9wwkK2VW0zgmG11boF3RMZkPTvRe8BSXrqzoz1Ud30NI8JUZjJ-GatDlAHSvBYYqpEe1Wn8v-YwXZsduZF-JNl0"
-                }
-            }
-            );
-            let json = await response.json();
-            setData(json.data);
-        } catch (error) {
-            console.error(error)
+  const getDataFromApiAsync = async () => {
+    try {
+      let response = await fetch(
+        "https://app.jala.tech/api/posts?per_page=15&page=1&with=creator",
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjcwY2UyYjZjNzEzNjkyZmVjNDEwMTJjODYwMjRhMzM4M2ZiOGZjNDI0NDU3NzFmMjMwNjIyMDFkOTJlNDZlYzIxNjYwNTk3ZjdlY2I4YzU4In0.eyJhdWQiOiIxIiwianRpIjoiNzBjZTJiNmM3MTM2OTJmZWM0MTAxMmM4NjAyNGEzMzgzZmI4ZmM0MjQ0NTc3MWYyMzA2MjIwMWQ5MmU0NmVjMjE2NjA1OTdmN2VjYjhjNTgiLCJpYXQiOjE2MzU5OTYyNzEsIm5iZiI6MTYzNTk5NjI3MSwiZXhwIjoxNjY3NTMyMjcxLCJzdWIiOiI3NyIsInNjb3BlcyI6W119.F1bxyaeDygJb66xI3gDYBWmoN_JObb4jItMEtGzDFuW2w9dlA_zglk0CbqZ-keBO3a3w75Ui3t5XoE5I2lV3PP0BMZC7fFgx00sicEVrzNFlt2oUzj5n3RgpGFnclJHmnX-ObSBk-1efciBdB0PcrSMjQp4HrhQXVkzN-Xd4debnzohNOX8nhqdf3GLOoQm8Fak6nSGWy0-vsY9J4mLjzNcPtkBA5lfPk9Z_TCzNUSy1iOyE8sZHcYQSGfehcXISOL1Oev_djgyVzzbZ45jW1GDujO4d94xqY2EdDDpPuKd2bMnA9FOgfoRrxvIJ8u1AFvr6A_QBzH1kwJfWems2_jlpF91C2ZYm1LQhf4DAMoaNZhd1SDYJheh_Nx8mgeYgBgqcWv2tD-1u0-ghyTfN950NelZ_IdUxDJ_Z9riLjDrDJ96WEJNp7pxHJfc34IM5Ok4Im1ewZr8VIIGoXY5u01in_af2JWpKfeODACPO7I2xheMX6c7NInDArMBByB31CUi_NvhJtDoYAZLCeNBWsAG3AGNapl5gue2EdYj263QrfadWtDp5scGaW1f1sINLo9e_HZceLplR336MABHy9wwkK2VW0zgmG11boF3RMZkPTvRe8BSXrqzoz1Ud30NI8JUZjJ-GatDlAHSvBYYqpEe1Wn8v-YwXZsduZF-JNl0",
+          },
         }
-    };
-    const [data, setData] = useState([])
-    useEffect(() => {
-        getDataFromApiAsync()
-    },[])
-    const renderItem = ({item}) => {
-        return(
-            <View style={styles.containerItem}>
-                <View style={styles.containerImg}>
-                    <Image
-                        source={{uri: "https://app.jala.tech/images/errors/404.png"}}
-                        style={styles.img}
-                    />
-                </View>
-                <View style = {styles.containerBody}>
-                    <Text style = {styles.txtTitle}>{item.title}</Text>
-                    <Text style = {styles.txtDesc}>{item.excerpt}</Text>
-                    <Text style = {styles.txtDesc}>{item.created_at}</Text>
-                </View>
-            </View>
-        )
+      );
+      let json = await response.json();
+      setData(json.data);
+    } catch (error) {
+      console.error(error);
     }
-    return(
-        <SafeAreaView style = {styles.container}>
-            <Text style = {styles.txtHeader}>Kabar Terbaru</Text>   
-                <FlatList
-                    data = {data}
-                    renderItem = {renderItem}
-                    keyExtractor = {(item) => item.id}
-                />
-        </SafeAreaView>
-    )
+  };
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getDataFromApiAsync();
+  }, []);
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.containerItem}>
+        <View style={styles.containerImg}>
+          <Image
+            source={{ uri: `https://app.jala.tech/storage/${item.image}` }}
+            style={styles.img}
+            
+          />
+        </View>
+        <View style={styles.containerBody}>
+          <Text style={styles.txtTitle}>{item.title}</Text>
+          <Text style={styles.txtDesc}>{item.excerpt}</Text>
+          <View style={styles.wrapperBodyFooter}>
+            <Text style={styles.txtDesc}>{moment(item.created_at).format("D MMMM YYYY")}</Text>
+            <TouchableOpacity onPress={onShare}>
+              <Ionicons
+                name="md-share-social"
+                size={25}
+                style={styles.iconFill}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message: `Bagikan profil supplier ${route.params.data.creator.name} `,
+        url: `https://app.jala.tech/shrimp_prices/${route.params.data.creator.id}`,
+        message: `Bagikan profil ${route.params.data.creator.name} `,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.txtHeader}>Kabar Terbaru</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-    },
-    txtHeader:{
-        fontSize: 18,
-        lineHeight: 24,
-        letterSpacing: 0.5,
-        fontWeight: "700",
-        alignSelf: "center",
-        padding: 10,
-        color:"#004492"
-    },
-    containerItem:{
-        flex: 1,
-        borderWidth: 0.5,
-        margin: 10,
-        borderRadius: 10
-    },
-    img:{
-        margin: 10,
-        height: 150,
-        width: 320,
-        resizeMode: "contain",
-        alignSelf: "center",
-        marginTop: -5
-    },
-    containerImg:{
-        flex: 1,
-        margin: 10,
-    },
-    containerBody:{
-        flex: 1,
-        margin: 10,
-        marginTop: -15
-    },
-    txtTitle: {
-        fontSize: 25,
-        margin: 10,
-        fontWeight: '600'
-    },
-    txtDesc: {
-        fontSize: 15,
-        margin: 10,
-        fontWeight: '300'
-    }
-})
+  container: {
+    flex: 1,
+  },
+  txtHeader: {
+    fontSize: 18,
+    lineHeight: 24,
+    letterSpacing: 0.5,
+    fontWeight: "700",
+    alignSelf: "center",
+    padding: 10,
+    color: "#004492",
+  },
+  containerItem: {
+    flex: 1,
+    borderWidth: 0.5,
+    margin: 10,
+    borderRadius: 10,
+  },
+  img: {
+    flex:1 ,
+    width: undefined,
+    height: undefined,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  containerImg: {
+    height: 150,
+    width: '100%',
+    marginBottom: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  containerBody: {
+    flex: 1,
+    margin: 10,
+    marginTop: -15,
+  },
+  wrapperBodyFooter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  txtTitle: {
+    fontSize: 14,
+    margin: 10,
+    fontWeight: "bold",
+  },
+  txtDesc: {
+    fontSize: 12,
+    margin: 10,
+  },
+  iconFill: {
+    color: "#545454",
+  },
+});
 
 export default ShrimpNews;
